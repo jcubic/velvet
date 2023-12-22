ebecea97933b2934509c89aba1003dc06504dcb0
Jakub Jankiewicz <jcubic@onet.pl>
2023-12-22 03:57:46 +0100

    add first version of the code

---
+++ crc32.ts
@@ -0,0 +1,31 @@
+// based on https://stackoverflow.com/a/18639999/387194
+const make_crc_table = () => {
+    let c: number;
+    const crc_table: Array<number> = [];
+    for(let n = 0; n < 256; n++){
+        c = n;
+        for(let k =0; k < 8; k++){
+            c = ((c&1) ? (0xEDB88320 ^ (c >>> 1)) : (c >>> 1));
+        }
+        crc_table[n] = c;
+    }
+    return crc_table;
+};
+
+const crc_table = make_crc_table();
+
+const crc32 = (str: string) => {
+    let crc = 0 ^ (-1);
+
+    for (let i = 0; i < str.length; i++ ) {
+        crc = (crc >>> 8) ^ crc_table[(crc ^ str.charCodeAt(i)) & 0xFF];
+    }
+
+    return (crc ^ (-1)) >>> 0;
+};
+
+const hash = (str: string): string => {
+    return crc32(str).toString(16);
+};
+
+export default hash;
---
+++ velvet.ts
@@ -0,0 +1,103 @@
+import hash from './crc32';
+
+const __DEBUG__ = false;
+
+type Style = Partial<CSSStyleDeclaration>;
+
+type StyleSheet<T> = {
+    readonly [key in keyof T]: Style
+};
+
+type ClassName = `velvet-${string}`;
+
+type ClassMapping<T> = {
+    readonly [key in keyof T]: ClassName
+};
+
+// The hash of the CSS in form of a class is keep as key, each time when we call inject
+// we increase the ref number and when destroy is called (fuction return from inject)
+// the ref is decreased when ref reach zero, the style is Removed from DOM
+const cache = new Map<ClassName, {ref: number, $style: HTMLStyleElement}>();
+
+const make_style = () => {
+    return document.createElement('style');
+};
+
+const empty_rule = (sheet: CSSStyleSheet, class_name: string) => {
+    const index = sheet.cssRules.length;
+    sheet.insertRule(`.${class_name} { }`, index);
+    return sheet.cssRules[index];
+}
+
+const set_css = (sheet: CSSStyleSheet, class_name: string, style: Style) => {
+    const selector = `.${class_name}`;
+    let rule = [...sheet.cssRules].find(rule => {
+        return rule instanceof CSSStyleRule && rule.selectorText === selector;
+    });
+    if (!rule) {
+        rule = empty_rule(sheet, class_name);
+    }
+    if (rule instanceof CSSStyleRule) {
+        Object.assign(rule.style, style);
+    }
+};
+
+const dump_css = (sheet: CSSStyleSheet) => {
+    return [...sheet.cssRules].map(rule => rule.cssText).join('\n')
+};
+
+const StyleSheet = {
+    create<T>(input: StyleSheet<T>): ClassMapping<T> {
+        const pairs: Array<[T, ClassName]> = Object.entries(input).map(([name, style]) => {
+            const repr = JSON.stringify(style);
+            const class_name: ClassName = `velvet-${hash(repr)}`;
+            if (cache.has(class_name)) {
+                const obj = cache.get(class_name)!;
+                obj.ref++;
+            } else {
+                const $style = make_style();
+                const { sheet } = $style;
+                set_css(sheet!, class_name, style as Style);
+                if (__DEBUG__) {
+                    // by default dynamic style is not visible in devtools
+                    $style.innerText = dump_css(sheet!);
+                }
+                cache.set(class_name, {
+                    ref: 1,
+                    $style
+                });
+            }
+            return [name as T, class_name as ClassName];
+        });
+        return Object.fromEntries(pairs);
+    }
+};
+
+export const inject = (class_name: ClassName, nonce?: string) => {
+    if (!cache.has(class_name)) {
+        throw new Error(`velvet: style with class ${class_name} not found`);
+    }
+    const obj = cache.get(class_name)!;
+    obj.ref++
+    if (nonce) {
+        // allow to use strict CSP (Content Security Policy)
+        obj.$style.nonce = nonce;
+    }
+    document.head.appendChild(obj.$style);
+    return (purge?: true) => {
+        if (cache.has(class_name)) {
+            const obj = cache.get(class_name)!;
+            obj.ref--;
+            // no more references we can remove the style from DOM
+            if (obj.ref <= 0) {
+                obj.$style.remove();
+                if (purge) {
+                    // when user decide he can also remove the style object
+                    // this is usesfull when the style will never be reused
+                    cache.delete(class_name);
+                }
+            }
+        }
+    };
+};
+
